be86599d57ea3cd2fa5d4c4cb17a40474bd837ef
LAPTOP-KV4DTCCF\chris <wenk0003@algonquinlive.com>
2018-03-13 16:00:54 -0400

    completed to step 21

---
+++ ChrisWandNowellaH/src/LayeredAppDemo/CoordConverterBLL/EnumsAndStructs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoordConverterBLL
+{
+    public enum Quadrant
+    {
+        N,
+        S,
+        E,
+        W
+    }
+    public struct DMSCoord
+    {
+        public int deg;
+        public int min;
+        public double sec;
+        Quadrant quadrant;
+    } 
+}
